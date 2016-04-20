029a07cbc281e9da94b6db6f14f37f02198ab0ad
Hila Vasiler <hila.marmel@gmail.com>
2016-04-20 12:18:29 -0700

    added PostsShowController get functionality

--- public/scripts/controllers/posts-new.controller.js
+++ public/scripts/controllers/posts-new.controller.js
@@ -1,5 +1,27 @@
 PostsNewController.$inject = ["$location", "$http"]; // minification protection
 function PostsNewController ($location, $http) {
   var vm = this;
-  vm.post = {}; // form data
+  vm.create = create;
+  vm.post = {
+    title:"",
+    content:""
+  }; // form data
+
+  function create(){
+    $http({
+      method: 'POST',
+      url:"/api/posts",
+      data: vm.post
+    }).then(onCreateSuccess, onCreateError);
+
+    function onCreateSuccess(response){
+      $location.path('/posts/' + response.data._id);
+    }
+
+    function onCreateError(response){
+      console.error("Failed to create post", response);
+    }
+  }
+
+//close PostsNewController
 }
--- public/scripts/controllers/posts-show.controller.js
+++ public/scripts/controllers/posts-show.controller.js
@@ -1,5 +1,22 @@
 PostsShowController.$inject = ["$location", "$http", "$routeParams"]; // minification protection
 function PostsShowController ($location, $http, $routeParams) {
   var vm = this;
-  vm.post = {};
+  vm.post = {
+    title: "",
+    content:""
+  };
+  $http({
+    method: 'GET',
+    url:'/api/posts/' + $routeParams.id
+  }).then(onGetSuccess, onGetError);
+
+  function onGetSuccess(response){
+    vm.post = response.data;
+  }
+  function onGetError(err){
+    console.error("Failed to create post", err);
+    $location.path('/');
+  }
+
+//close PostsShowController
 }
